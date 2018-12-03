9da35a536dd562f2a45a81561463145a1cd9d6de
alvinjo <alvin.joseph0@gmail.com>
2018-12-03 18:27:09 +0000

    setup does everything

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
+#Get the tools
 sudo yum install git -y
 sudo yum install docker -y
 sudo yum install docker-compose -y
 sudo yum install maven -y
 
+#Pull, configure and pack
 bash ./gitinit.sh
 bash ./gitpullrecent.sh
 bash ./deletetests.sh
 bash ./copydockerfiles.sh
+bash ./adjustappprop.sh
 bash ./mvncleanpackall.sh
+
+#Beam it up scotttyyyy
+bash ./beammeup.sh
